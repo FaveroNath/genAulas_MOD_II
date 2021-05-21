aa4b939a6483d757d2b20922d805779377e78ade
FaveroNath <faveronathalia@gmail.com>
2021-05-21 15:14:24 -0300

    Ex 2 e 3 referente a aula de Join

---
+++ Joins/script_ex3.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE db_farmacia_do_bem;
+
+USE db_farmacia_do_bem;
+
+CREATE TABLE tb_categoria(
+	id BIGINT(5) AUTO_INCREMENT,
+    tipo VARCHAR(255) NOT NULL,
+    generico BOOLEAN NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE tb_produto(
+	id BIGINT(5) AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10,2),
+    descricao VARCHAR(255) NULL,
+    categoria_id BIGINT(5)  NOT NULL,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY(categoria_id) REFERENCES tb_categoria(id)
+    
+);
+
+INSERT INTO tb_categoria(tipo,generico)
+VALUES 
+	("Cosmético",0),
+    ("Medicamentos", true),
+    ("Medicamentos",false),
+    ("Higiene e beleza",0),
+    ("Conveniência",0);
+    
+INSERT INTO tb_produto(nome,preco,descricao,categoria_id)
+VALUES
+	("Dipirona",3.30,"500 mg",2),
+    ("Dipirona",10.30,"1 g",3),
+	("Batom", 15.50,"cor de rosa",1),
+    ("cetoprofeno", 2.30,"100 mg",2),
+    ("Ibuprofeno", 7.30,"Gostas",3),
+    ("Secador", 150.90 ,"Seca cabelos",4),
+    ("Sabonete liguído nivea",61.30,"Anti acne",4),
+    ("Balinha de canela", 1.30,"canela",5);
+
+-- Faça um select que retorne os Produtos com o valor maior do que 50 reais.
+SELECT * FROM tb_produto WHERE preco > 50;
+
+-- Faça um select trazendo os Produtos com valor entre 3 e 60 reais.
+SELECT * FROM tb_produto WHERE preco BETWEEN 3 AND 60;
+
+-- Faça um select utilizando LIKE buscando os Produtos com a letra B.
+SELECT * FROM tb_produto WHERE nome LIKE "b%";
+SELECT * FROM tb_produto WHERE nome LIKE "%b%";
+SELECT * FROM tb_produto WHERE nome LIKE "%b";
+
+-- Faça um um select com Inner join entre tabela categoria e produto.
+SELECT tb_produto.nome, tb_produto.preco, tb_produto.descricao, tb_categoria.tipo, tb_categoria.generico AS Medicamento_Generico FROM tb_produto 
+INNER JOIN tb_categoria 
+ON tb_categoria.id = tb_produto.categoria_id;
+
+
+-- Faça um select onde traga todos os Produtos de uma categoria específica (exemplo todos os produtos que são cosméticos).
+SELECT * FROM tb_produto
+INNER JOIN tb_categoria 
+ON tb_categoria.id = tb_produto.categoria_id
+WHERE tb_categoria.tipo = "Cosmético";
+
+SELECT * FROM tb_categoria
---
+++ Joins/script_ex4.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE db_cidade_das_frutas;
+
+USE db_cidade_das_frutas;
+
+CREATE TABLE tb_categoria(
+	id BIGINT(5) AUTO_INCREMENT,
+    tipo VARCHAR(255) NOT NULL,
+    graos BOOLEAN,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE tb_produto(
+	id BIGINT(5) AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10,2),
+    quantidade BIGINT(5) NULL,
+    categoria_id BIGINT(5)  NOT NULL,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY(categoria_id) REFERENCES tb_categoria(id)
+    
+);
+
+INSERT INTO tb_categoria(tipo,graos)
+VALUES
+	("Legumes",false),
+    ("Verduras",false),
+    ("Não perecíveis", true),
+    ("Frutas",false),
+    ("temperos",false);
+    
+INSERT INTO tb_produto(nome,preco,quantidade,categoria_id)
+VALUES
+	("Cenoura",6.35,300,1),
+    ("Batata", 2.35,550,1),
+    ("Beterraba",8.35,150,1),
+    ("Couve flor",5.35,881,2),
+    ("Orégano",12.00,984,5),
+    ("Laranja", 9.35,50 , 4),
+    ("Lentilha",70.99,1001,3),
+    ("Coco",60.35,123,4);
+    
+-- Faça um select que retorne os Produtos com o valor maior do que 50 reais.
+SELECT * FROM tb_produto WHERE preco > 50;
+
+-- Faça um select trazendo os Produtos com valor entre 3 e 60 reais.
+SELECT * FROM tb_produto WHERE preco  BETWEEN 3 AND 60;
+
+-- Faça um select utilizando LIKE buscando os Produtos com a letra C.
+SELECT * FROM tb_produto WHERE nome LIKE "c%";
+SELECT * FROM tb_produto WHERE nome LIKE "%c%";
+SELECT * FROM tb_produto WHERE nome LIKE "%c";
+
+-- Faça um um select com Inner join entre tabela categoria e produto.
+SELECT * FROM tb_produto 
+INNER JOIN tb_categoria 
+ON tb_categoria.id = tb_produto.categoria_id;
+
+-- Faça um select onde traga todos os Produtos de uma categoria específica (exemplo todos os produtos que são aves ou legumes).
+SELECT * FROM tb_produto 
+INNER JOIN tb_categoria 
+ON tb_categoria.id = tb_produto.categoria_id
+WHERE tb_categoria.tipo = "Frutas";
+
+SELECT * FROM tb_categoria;    
+
+
